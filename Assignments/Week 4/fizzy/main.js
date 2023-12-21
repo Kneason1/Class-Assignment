1021dd4300e24acff51e37948f3a4b8dc77bbe33
Kneason1 <keason.prsvr@gmail.com>
2023-12-20 20:45:49 -0600

    Completed Week 3

--- Assignments/Week 4/fizzy/main.js
+++ Assignments/Week 4/fizzy/main.js
@@ -1,7 +1,15 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
-
+if(x % 3 === 0 && x % 5 === 0){
+  return "FizzBuzz"}
+else if(x % 3 === 0){
+  return "Fizz"
+} else if(x % 5 === 0){
+  return "Buzz"
+} else{
+  return x
+}
 
 
 
